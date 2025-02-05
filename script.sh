c7d40c8418175bb3ee07a6e00dce94835e0b9993
Pragati Narote <pragatinarote@Pragatis-MacBook-Air.local>
2025-02-05 10:35:18 -0500

    Adds shell script for setting up the application

---
+++ script.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+APP_DIR="/opt/csye6225"
+WEBAPP_DIR="/opt/csye6225/webapp"
+ZIP_FILE="/root/webapp.zip"
+ENV_FILE="/root/.env"
+
+sudo apt update -y
+
+sudo apt upgrade -y
+
+sudo apt install -y git curl postgresql postgresql-contrib nodejs npm unzip
+
+sudo systemctl start postgresql 
+sudo systemctl enable postgresql
+
+echo "Creating PostgreSQL database and user..." 
+sudo -i -u postgres psql <<EOF 
+CREATE DATABASE healthcheck_db; 
+CREATE USER app_user; 
+GRANT ALL PRIVILEGES ON DATABASE healthcheck_db TO app_user; 
+EOF
+
+
+echo "Creating application user and group..."
+sudo groupadd -f healthapp_group
+sudo useradd -m -g healthapp_group -s /bin/bash healthapp_user
+
+echo "Setting up application directory..."
+sudo rm -rf $APP_DIR
+sudo mkdir -p $APP_DIR
+sudo unzip -o $ZIP_FILE -d $APP_DIR
+
+echo "Moving .env file to the application directory..." 
+sudo mv $ENV_FILE $WEBAPP_DIR/
+
+echo "Updating permissions..." 
+sudo chown -R healthapp_user:healthapp_group $APP_DIR 
+sudo chmod -R 750 $APP_DIR
+
+echo "Installing Node.js dependencies in /opt/csye6225/webapp..." 
+cd $WEBAPP_DIR 
+sudo npm install
+
+echo "Application setup completed!"
