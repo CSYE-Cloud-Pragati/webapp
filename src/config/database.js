683c4edcd30d185261e96c16513ab1229f3113d3
Pragati Narote <pragatinarote@Pragatis-MacBook-Air.local>
2025-03-20 01:50:29 -0400

    SSL enabled for test

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+// Determine if we are in the test environment
+const isTestEnv = process.env.NODE_ENV === 'test';
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USERNAME,
@@ -9,12 +12,14 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, 
-      },
-    },
+    dialectOptions: isTestEnv
+      ? {} 
+      : {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
     logging: false,
   }
 );
